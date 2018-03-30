3ecb89d11490c49c9256b711ace8c87dae805528
Charlotte Rouby <charlotte.rouby@hotmail.fr>
2018-03-30 19:47:14 +0200

    exo 14 Display the user

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,21 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import { tap } from 'rxjs/operators';
+import { UserModel } from './models/user.model';
 
 @Injectable()
 export class UserService {
 
   private _configUrl = 'http://ponyracer.ninja-squad.com';
 
+  userEvents: Subject<UserModel> = new Subject<UserModel>();
+
   constructor(private http: HttpClient) { }
 
-  register(login: string, password: string, birthYear: number): Observable<Object> {
+  register(login: string, password: string, birthYear: number): Observable<UserModel> {
     const params = {login, password, birthYear};
-    return this.http.post<Object>(`${this._configUrl}/api/users`, params );
+    return this.http.post<UserModel>(`${this._configUrl}/api/users`, params );
   }
 
-  authenticate(credentials: {login: string; password: string}): Observable<Object> {
-    return this.http.post<Object>(`${this._configUrl}/api/users/authentication`, credentials);
+  authenticate(credentials: {login: string; password: string}): Observable<UserModel> {
+    return this.http.post<UserModel>(`${this._configUrl}/api/users/authentication`, credentials).pipe(
+      tap(user => this.userEvents.next(user))
+    );
   }
 
 }
