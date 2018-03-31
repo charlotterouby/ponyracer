1dfdca955244fa1465461fb7758a2965e7e95e1b
Charlotte Rouby <charlotte.rouby@hotmail.fr>
2018-03-31 16:42:35 +0200

    exo 16. Remember me

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -12,17 +12,33 @@ export class UserService {
 
   userEvents: BehaviorSubject<UserModel> = new BehaviorSubject<UserModel>(undefined);
 
-  constructor(private http: HttpClient) { }
-
-  register(login: string, password: string, birthYear: number): Observable<UserModel> {
-    const params = {login, password, birthYear};
-    return this.http.post<UserModel>(`${this._configUrl}/api/users`, params );
+  constructor(private http: HttpClient) {
+    this.retrieveUser();
   }
 
-  authenticate(credentials: {login: string; password: string}): Observable<UserModel> {
+  register(login: string, password: string, birthYear: number): Observable<UserModel> {
+    const params = { login, password, birthYear };
+    return this.http.post<UserModel>(`${this._configUrl}/api/users`, params);
+  }
+
+  authenticate(credentials: { login: string; password: string }): Observable<UserModel> {
     return this.http.post<UserModel>(`${this._configUrl}/api/users/authentication`, credentials).pipe(
-      tap(user => this.userEvents.next(user))
+      tap(user => this.storeLoggedInUser(user))
     );
+  }
+
+  storeLoggedInUser(user: UserModel) {
+    this.userEvents.next(user);
+    window.localStorage.setItem('rememberMe', JSON.stringify(user));
+  }
+
+  retrieveUser() {
+    const userRememberMe = window.localStorage.getItem('rememberMe');
+
+    if (userRememberMe) {
+      const user: UserModel = JSON.parse(userRememberMe);
+      this.userEvents.next(user);
+    }
   }
 
 }
