b613763406d60d814f44cfc70b9765b2e7e1b505
Charlotte Rouby <charlotte.rouby@hotmail.fr>
2018-10-14 11:16:00 +0200

    [exo 5] Observables with RxJS

--- src/app/race.service.ts
+++ src/app/race.service.ts
@@ -1,22 +1,22 @@
 import { Injectable } from '@angular/core';
 
 import { RaceModel } from './models/race.model';
+import { Observable, of } from 'rxjs';
+import {delay} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RaceService {
-
   constructor() {}
 
-  list(): Array<RaceModel> {
-    return [
+  list(): Observable<Array<RaceModel>> {
+    return of([
       { name: 'Lyon' },
       { name: 'Los Angeles' },
       { name: 'Sydney' },
       { name: 'Tokyo' },
       { name: 'Casablanca' }
-    ];
+    ]).pipe(delay(500));
   }
-
 }
